449b5e910f40042da68c8a99abb405a32be4a599
--get <reallyasi9@users.noreply.github.com>
2024-05-30 10:15:23 -0400

    Add LZOPCompressor constructor test

--- src/CodecLZOP.jl
+++ src/CodecLZOP.jl
@@ -8,6 +8,11 @@ using TranscodingStreams
 
 const _crc32 = CRC32.crc32
 
+export LZOPCompressor
+
+# re-export LZOP-supported LibLZO compressor types
+export LZO1X, LZO1X_1, LZO1X_1_11, LZO1X_999
+
 include("block.jl")
 include("lzop_compressor.jl")
 include("lzop_decompressor.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,70 @@ using Test
 using TestItemRunner
 
 @testitem "LZOPCompressor constructors" begin
-    
+    # default constructor
+    c = LZOPCompressor()
+    @test typeof(c) == LZOPCompressor{LZO1X_1,typeof(identity)}
+    @test typeof(c.algo) == LZO1X_1
+    @test c.block_size == CodecLZOP.LZOP_DEFAULT_BLOCK_SIZE
+    @test c.crc32 == true
+    @test c.filter_fun == identity
+    @test c.optimize == false
+
+    # kwarg constructor
+    algo = LZO1X_1_11()
+    block_size = 1
+    crc32 = false
+    filter_fun = Base.:-
+    optimize = true
+    c = LZOPCompressor(algo; block_size=block_size, crc32=crc32, filter=filter_fun, optimize=optimize)
+    @test typeof(c) == LZOPCompressor{typeof(algo),typeof(Base.:-)}
+    @test typeof(c.algo) == typeof(algo)
+    @test c.block_size == block_size
+    @test c.crc32 == crc32
+    @test c.filter_fun == Base.:-
+    @test c.optimize == optimize
+
+    # algo type constructor
+    algo = LZO1X_1_11
+    block_size = 1
+    crc32 = false
+    filter_fun = Base.:-
+    optimize = true
+    c = LZOPCompressor(algo; block_size=block_size, crc32=crc32, filter=filter_fun, optimize=optimize)
+    @test typeof(c) == LZOPCompressor{algo,typeof(Base.:-)}
+    @test typeof(c.algo) == algo
+    @test c.block_size == block_size
+    @test c.crc32 == crc32
+    @test c.filter_fun == Base.:-
+    @test c.optimize == optimize
+
+    # algo symbol constructor
+    algo = :LZO1X_1_11
+    block_size = 1
+    crc32 = false
+    filter_fun = Base.:-
+    optimize = true
+    c = LZOPCompressor(algo; block_size=block_size, crc32=crc32, filter=filter_fun, optimize=optimize)
+    @test typeof(c) == LZOPCompressor{LZO1X_1_11,typeof(Base.:-)}
+    @test typeof(c.algo) == LZO1X_1_11
+    @test c.block_size == block_size
+    @test c.crc32 == crc32
+    @test c.filter_fun == Base.:-
+    @test c.optimize == optimize
+
+    # algo string constructor
+    algo = "LZO1X_1_11"
+    block_size = 1
+    crc32 = false
+    filter_fun = Base.:-
+    optimize = true
+    c = LZOPCompressor(algo; block_size=block_size, crc32=crc32, filter=filter_fun, optimize=optimize)
+    @test typeof(c) == LZOPCompressor{LZO1X_1_11,typeof(Base.:-)}
+    @test typeof(c.algo) == LZO1X_1_11
+    @test c.block_size == block_size
+    @test c.crc32 == crc32
+    @test c.filter_fun == Base.:-
+    @test c.optimize == optimize
 end
 
 @testitem "LZOPDecompressor constructors" begin
