e35bf5928425a4b79b5d79f4bdd828e5d8f23de3
--get <reallyasi9@users.noreply.github.com>
2024-06-26 00:27:42 -0400

    Fix minsize off-by-one error

--- src/lzop_compressor.jl
+++ src/lzop_compressor.jl
@@ -90,9 +90,8 @@ function TranscodingStreams.minoutsize(codec::LZOPCompressor, input::Transcoding
     # Uncompressed checksum, compressed checksum: each a UInt32.
     # You only get the compressed checksum if compressed length < uncompressed length.
     # And compressed length <= uncompressed length, always
-    # Thus the maximum number of bytes occurs when each input block compresses by exactly one byte, thereby increasing the total size by 15 bytes per block.
     d = length(input) ÷ codec.block_size
-    extra = 8 + (!isnothing(codec.uncompressed_checksum) ? 4 : 0) + (!isnothing(codec.compressed_checksum) ? 4 : 0) - 1
+    extra = 8 + (!isnothing(codec.uncompressed_checksum) ? 4 : 0) + (!isnothing(codec.compressed_checksum) ? 4 : 0)
     return length(input) + (d + 1) * extra
 end
 
